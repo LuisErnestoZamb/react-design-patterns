feb6aaaafb8d4c24b1b34e826d5589861f6e3b19
Luis Zambrano <luisezamb@gmail.com>
2024-06-06 19:39:59 -0400

    sending component as child

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Modal } from "./Modal";
 import { NumberedList } from "./NumberedList";
 import { LargePersonListItem } from "./people/LargePersonListItem";
 import { SmallPersonListItem } from "./people/SmallPersonListItem";
@@ -6,43 +7,46 @@ import { SmallProductListItem } from "./products/SmallProductListItem";
 import { RegularList } from "./RegularList";
 
 const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
+  name: 'John Doe',
+  age: 54,
+  hairColor: 'brown',
+  hobbies: ['swimming', 'bicycling', 'video games'],
 }, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
+  name: 'Brenda Smith',
+  age: 33,
+  hairColor: 'black',
+  hobbies: ['golf', 'mathematics'],
 }, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
+  name: 'Jane Garcia',
+  age: 27,
+  hairColor: 'blonde',
+  hobbies: ['biology', 'medicine', 'gymnastics'],
 }];
 
 const products = [{
-	name: 'Flat-Screen TV',
-	price: '$300',
-	description: 'Huge LCD screen, a great deal',
-	rating: 4.5,
+  name: 'Flat-Screen TV',
+  price: '$300',
+  description: 'Huge LCD screen, a great deal',
+  rating: 4.5,
 }, {
-	name: 'Basketball',
-	price: '$10',
-	description: 'Just like the pros use',
-	rating: 3.8,
+  name: 'Basketball',
+  price: '$10',
+  description: 'Just like the pros use',
+  rating: 3.8,
 }, {
-	name: 'Running Shoes',
-	price: '$120',
-	description: 'State-of-the-art technology for optimum running',
-	rating: 4.2,
+  name: 'Running Shoes',
+  price: '$120',
+  description: 'State-of-the-art technology for optimum running',
+  rating: 4.2,
 }];
 
 function App() {
-	return (
-		<>
-		<RegularList
+  return (
+    <>
+      <Modal>
+        <LargeProductListItem product={products[0]} />
+      </Modal>
+      {/* <RegularList
 			items={people}
 			resourceName="person"
 			itemComponent={SmallPersonListItem} />
@@ -57,9 +61,9 @@ function App() {
 		<NumberedList
 			items={products}
 			resourceName="product"
-			itemComponent={LargeProductListItem} />
-		</>
-	);
+			itemComponent={LargeProductListItem} /> */}
+    </>
+  );
 }
 
 export default App;
