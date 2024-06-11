6d49fa9a06cf0b14cc4cb64f947e7a766d4b51bd
Luis Zambrano <luisezamb@gmail.com>
2024-06-10 21:31:07 -0400

    Making the container component

---
+++ src/UnControlledOnboardingFlow.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export const UnControlledOnboardingFlow = ({ children, onFinish }) => {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNext = () => {
+    setCurrentIndex(currentIndex + 1);
+  }
+
+  const currentChild = React.Children.toArray(children)[currentIndex];
+
+  if (React.isValidElement(currentChild))
+    return React.cloneElement(currentChild, { goToNext });
+  return currentChild;
+};
