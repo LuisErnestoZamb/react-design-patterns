605e7d03067584fa9c0fe7fa657ce6466ae8b4c5
Luis Zambrano <luisezamb@gmail.com>
2024-06-10 22:09:13 -0400

    Showing compnent according to props

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 
 const StepOne = ({ goToNext }) => (
 	<>
-	<h1>Step 1</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 1</h1>
+		<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
 	</>
 );
 const StepTwo = ({ goToNext }) => (
 	<>
-	<h1>Step 2</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 2</h1>
+		<button onClick={() => goToNext({ age: 100 })}>Next</button>
 	</>
 );
 const StepThree = ({ goToNext }) => (
 	<>
-	<h1>Step 3</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 3</h1>
+		<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
 	</>
 );
 
 function App() {
 	return (
-		<UncontrolledOnboardingFlow>
+		<UncontrolledOnboardingFlow
+			onFinish={data => {
+				console.log(data)
+				alert('Onboarding completed');
+			}}
+		>
 			<StepOne />
 			<StepTwo />
 			<StepThree />
--- src/UncontrolledOnboardingFlow.js
+++ src/UncontrolledOnboardingFlow.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 
 export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
-	const [onboardingData, setOnboardingData] = useState({});
-	const [currentIndex, setCurrentIndex] = useState(0);
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	const goToNext = stepData => {
-		setCurrentIndex(currentIndex + 1);
-	}
+  const goToNext = stepData => {
+    const nextIndex = currentIndex + 1;
+    const updatedData = {
+      ...onboardingData, ...stepData,
+    };
 
-	const currentChild = React.Children.toArray(children)[currentIndex];
+    console.log(updatedData);
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData)
+    }
 
-	if (React.isValidElement(currentChild)) {
-		return React.cloneElement(currentChild, { goToNext });
-	}
+    setOnboardingData(updatedData);
+  }
 
-	return currentChild;
+  const currentChild = React.Children.toArray(children)[currentIndex];
+
+  if (React.isValidElement(currentChild)) {
+    return React.cloneElement(currentChild, { goToNext });
+  }
+
+  return currentChild;
 }
