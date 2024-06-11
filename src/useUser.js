f072b3947540b6aec6e0e7ca594e41cfdd6b60b3
Luis Zambrano <luisezamb@gmail.com>
2024-06-11 14:43:09 -0400

    reusable userId

---
+++ src/useUser.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+export const useUser = userId => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+
+
+    (async () => {
+      const response = await axios.get(`/users/${userId}`)
+      setUser(response.data);
+    })();
+  }, [userId]);
+
+  return user;
+};
